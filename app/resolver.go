d4df7d3ef7919b3b5112b233683fd841f96eefd1
Raphaël P. Barazzutti <raphael@barazzutti.net>
2021-12-20 23:13:16 +0100

    net.Resolver's PreferGo isn't respected on Windows, fix

--- app/resolver.go
+++ app/resolver.go
@@ -1,9 +1,9 @@
 package app
 
 import (
-	"context"
 	"fmt"
 	"github.com/domainr/dnsr"
+	"github.com/miekg/dns"
 	"net"
 	"strings"
 )
@@ -31,6 +31,87 @@ func (resolver *resolver) resolveConn(addr string) (string, error) {
 		}
 		return fmt.Sprintf("%s:%s", resolved, port), nil
 	}
+}
+
+func resolveWithSpecificServerInternal(network, server string, host string) ([]*net.IP, error) {
+	var ips []*net.IP
+
+	msg := new(dns.Msg)
+	msg.Id = dns.Id()
+	msg.RecursionDesired = true
+	msg.Question = []dns.Question{}
+
+	if network == "ip6" {
+		msg.Question = append(msg.Question, dns.Question{Name: host, Qtype: dns.TypeAAAA, Qclass: dns.ClassINET})
+	} else if network == "ip4" {
+		msg.Question = append(msg.Question, dns.Question{Name: host, Qtype: dns.TypeA, Qclass: dns.ClassINET})
+	}
+
+	c := new(dns.Client)
+
+	in, _, err := c.Exchange(msg, fmt.Sprintf("%s:53", server))
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, a := range in.Answer {
+		if ipv4, ok := a.(*dns.A); ok {
+			ips = append(ips, &ipv4.A)
+		} else if ipv6, ok := a.(*dns.AAAA); ok {
+			ips = append(ips, &ipv6.AAAA)
+		}
+	}
+	return ips, nil
+}
+
+func resolveWithSpecificServer(network, server string, host string) ([]*net.IP, error) {
+
+	type resolveAnswer struct {
+		ip  []*net.IP
+		err error
+	}
+	if network == "ip" {
+		var ips []*net.IP
+
+		answersChan := make(chan *resolveAnswer)
+		ret := func(prot string) {
+			out, err := resolveWithSpecificServerInternal(prot, server, host)
+
+			answersChan <- &resolveAnswer{out, err}
+
+		}
+		go ret("ip4")
+		go ret("ip6")
+
+		var answers []*resolveAnswer
+
+		oneSucceeded := false
+		for i := 0; i < 2; i++ {
+			answer := <-answersChan
+
+			if answer.err == nil {
+				oneSucceeded = true
+			}
+
+			answers = append(answers, answer)
+
+		}
+
+		if !oneSucceeded {
+			return nil, &net.DNSError{Err: "no such host", Name: host, IsNotFound: true}
+		}
+
+		for _, answer := range answers {
+			if answer.err == nil {
+				ips = append(ips, answer.ip...)
+			}
+		}
+
+		return ips, nil
+	}
+	return resolveWithSpecificServerInternal(network, server, host)
+
 }
 
 func (resolver *resolver) resolve(addr string) (*net.IPAddr, error) {
@@ -64,29 +145,19 @@ func (resolver *resolver) actualResolve(addr string) (*net.IPAddr, error) {
 		}
 		return &net.IPAddr{IP: ip}, nil
 	} else if resolver.config.DNSServer != "" {
-		dnsTarget := fmt.Sprintf("%s:53", resolver.config.DNSServer)
-
-		r := &net.Resolver{
-			PreferGo: true,
-			Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
-				d := net.Dialer{}
-
-				return d.DialContext(ctx, network, dnsTarget)
-			},
-		}
-
-		ip, err := r.LookupIP(context.Background(), resolver.config.IPProtocol, addr)
+		ip, err := resolveWithSpecificServer(resolver.config.IPProtocol, resolver.config.DNSServer, fmt.Sprintf("%s.", addr))
 		if err != nil {
-			if dnsError, ok := err.(*net.DNSError); ok {
-				dnsError.Server = ""
-			}
 			return nil, err
 		}
 
-		return &net.IPAddr{IP: ip[0]}, nil
+		if len(ip) == 0 {
+			return nil, &net.DNSError{Err: "no such host", Name: addr, IsNotFound: true}
+		}
 
+		return &net.IPAddr{IP: *ip[0]}, nil
+	} else {
+		return net.ResolveIPAddr(resolver.config.IPProtocol, addr)
 	}
-	return net.ResolveIPAddr(resolver.config.IPProtocol, addr)
 }
 
 func (*resolver) fullResolveFromRoot(network, host string) (*string, error) {
