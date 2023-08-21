4ede49c474f556f00fcc7cf3f1ac3bdbf6ed395e
Raphaël P. Barazzutti <raphael@barazzutti.net>
2023-08-21 13:10:52 +0200

    2023!

--- stats/timer.go
+++ stats/timer.go
@@ -1,4 +1,4 @@
-// Copyright 2022 Raphaël P. Barazzutti
+// Copyright 2022-2023 - Raphaël P. Barazzutti
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
